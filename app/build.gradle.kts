1a98463ac3fb45be1fb35cc1b978257444f44f54
Saubhagya <saubhagyaranjanbehera64@gmail.com>
2025-03-22 12:41:05 +0530

    feat: Enhance app setup and UI elements
    This commit focuses on improving the app's initial setup, dependency management, and UI elements.
    
    Key changes:
    
    -   **Dependencies:**
        -   Updated the Kotlin version to "2.1.10" in `gradle/libs.versions.toml` and `.idea/kotlinc.xml`.
        - Updated the agp to 8.8.2
        - Added  the gson, and the coil dependencies.
    -   - Added the INTERNET permission in the `AndroidManifest.xml`.
    - **UI Improvements:**
        -   Added a custom font `futura_bold.ttf`.
        -   Applied the custom font `@font/product_sans_regular` and `@font/futura_bold` to the `AppTheme` in `themes.xml`.
        - Added more strings resources in the `strings.xml`
        - Added the activity for the home screen in `AndroidManifest.xml`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation("com.google.code.gson:gson:2.12.1")
+
+
+    //coil
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
 }
