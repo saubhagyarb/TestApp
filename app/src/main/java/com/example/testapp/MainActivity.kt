53276b2a07c8c97d3f4415a4239e81fb6f26961b
Saubhagya <saubhagyaranjanbehera64@gmail.com>
2025-03-29 16:21:21 +0530

    feat: Enhance rating UI and adjust system bar insets
    This commit focuses on improving the user interface by adding a `contentDescription` to the rating star and adjusting system bar insets for better display.
    
    Key changes:
    
    -   **Rating UI Enhancement:**
        - Added `contentDescription="@string/rating_star"` to the rating star `ImageView` in `movie_item.xml`, making it more accessible.
        - Added a `rating_star` string in `strings.xml` file.
    -   **System Bar Insets Adjustment:**
        - Updated the `MainActivity` to include `WindowInsetsCompat.Type.displayCutout()` when setting the system bar insets.

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -11,12 +11,19 @@ import com.google.android.material.button.MaterialButton
 class MainActivity : AppCompatActivity() {
     lateinit var getStartedBtn : MaterialButton
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars() or
+            WindowInsetsCompat.Type.displayCutout())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
